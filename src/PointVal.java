12c9a6a0b2e2ab2e33331d9601ab8fe7a3783939
Aidan Daly <aidandaly@dhcp-0059515512-82-a2.client.student.harvard.edu>
2010-04-15 20:43:45 -0400

    Foo

---
+++ src/PointVal.java
@@ -0,0 +1,11 @@
+public class PointVal 
+{
+  public final Color c;
+  public final int val;
+  
+  public PointVal(Color c, int val)
+  {
+    this.c = c;
+    this.val = val;
+  }
+}
---
+++ src/PointVal.java~
@@ -0,0 +1,11 @@
+public class Cost 
+{
+  public final Color c;
+  public final int val;
+  
+  public Cost(Color c, int val)
+  {
+    this.c = c;
+    this.val = val;
+  }
+}
