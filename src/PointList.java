a2f527e5977c02e3e44adbbde713197a62154935
Aidan Daly <aidandaly@dhcp-0059515512-82-a2.client.student.harvard.edu>
2010-04-16 00:35:01 -0400

    Bar

--- src/PointList.java
+++ src/PointList.java
@@ -2,5 +2,8 @@ public interface PointList
 {
   public boolean canPay(PointVal p);
   public void pay(PointList p);
+  public void pay(PointVal p);
   public void add(PointList p);
+  public void add(PointVal p);
+  public int get(Color c);
 }
