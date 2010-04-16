f63d595efa27606d357ad716f75e09a5f8d4feb9
Aidan Daly <aidandaly@dhcp-0059515512-82-a2.client.student.harvard.edu>
2010-04-15 20:40:15 -0400

    Foo

--- src/Card.java
+++ src/Card.java
@@ -1,12 +1,11 @@
 public interface Card
 {
-  public CostList getCost();
+  public PointList getCost();
   public Ability getAbility();
   
   /* For now, each card will only have one ability */
   public void activateAbility();
   
   public Boolean isActive();
-  public 
   
 }
