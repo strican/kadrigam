e3760c1725ca7d9a5a6951d3d11364b76d245516
Aidan Daly <aidandaly@dhcp-0059515512-82-a2.client.student.harvard.edu>
2010-05-03 16:08:55 -0400

    CardPanel displays scaled images

--- src/Effect.java
+++ src/Effect.java
@@ -10,4 +10,27 @@ public class Effect implements Serializable
     this.mag = mag;
     this.type = type;
   }
+
+  @Override
+  public String toString()
+  {
+      String s = "";
+      s += type+" "+mag;
+      switch(type) {
+          case HEAL:
+          case DEALDAMAGE:
+              s+=" POINTS\n";
+              break;
+          case DISCARD:
+          case DRAW:
+              s+=" CARDS\n";
+              break;
+          case DEACTIVATE:
+          case DESTROY:
+              s+= " CREATURES\n";
+              break;
+          default: s+="";
+      }
+      return s;
+  }
 }
